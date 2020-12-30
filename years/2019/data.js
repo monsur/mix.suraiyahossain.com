815ac660fe524704863a657b7ab4977e3314d960
Monsur Hossain <monsur@gmail.com>
2020-12-30 12:11:09 -0600

    fixing artist/title in data file

--- years/2019/data.js
+++ years/2019/data.js
@@ -5,80 +5,80 @@ var _DATA = {
   spotify: 'https://open.spotify.com/playlist/1VUZxHGBpDSF3NqpZw6aUH?si=UFhrqqaWSdaDHipUT-7Gow',
   tracks: [
     { src: '01-Lizzo-TruthHurts.mp3',
-      title: 'Lizzo',
-      artist: 'Truth Hurts'
+      title: 'Truth Hurts',
+      artist: 'Lizzo'
     },
     { src: '02-ShawnMendes-LostInJapan.mp3',
-      title: 'Shawn Mendes',
-      artist: 'Lost in Japan'
+      title: 'Lost in Japan',
+      artist: 'Shawn Mendes'
     },
     { src: '03-KatyPerry-NeverReallyOver.mp3',
-      title: 'Katy Perry',
-      artist: 'Never Really Over'
+      title: 'Never Really Over',
+      artist: 'Katy Perry'
     },
     { src: '04-JoyCrookes-LondonMine.mp3',
-      title: 'Joy Crookes',
-      artist: 'London Mine'
+      title: 'London Mine',
+      artist: 'Joy Crookes'
     },
     { src: '05-MaggieRogers-Alaska.mp3',
-      title: 'Maggie Rogers',
-      artist: 'Alaska'
+      title: 'Alaska',
+      artist: 'Maggie Rogers'
     },
     { src: '06-Clairo-Bags.mp3',
-      title: 'Clairo',
-      artist: 'Bags'
+      title: 'Bags',
+      artist: 'Clairo'
     },
     { src: '07-LanaDelRey-TheGreatest.mp3',
-      title: 'Lana Del Rey',
-      artist: 'The greatest'
+      title: 'The greatest',
+      artist: 'Lana Del Rey'
     },
     { src: '08-CarlyRaeJespen-TooMuch.mp3',
-      title: 'Carly Rae Jespen',
-      artist: 'Too Much'
+      title: 'Too Much',
+      artist: 'Carly Rae Jespen'
     },
     { src: '09-EdSheeran-IDontCare.mp3',
-      title: 'Ed Sheeran (with Justin Bieber)',
-      artist: 'I Don\'t Care'
+      title: 'I Don\'t Care',
+      artist: 'Ed Sheeran (with Justin Bieber)'
     },
     { src: '10-HAIM-SummerGirl.mp3',
-      title: 'HAIM',
-      artist: 'Summer Girl'
+      title: 'Summer Girl',
+      artist: 'HAIM'
     },
     { src: '11-VampireWeekend-ThisLife.mp3',
-      title: 'Vampire Weekend',
-      artist: 'This Life'
+      title: 'This Life',
+      artist: 'Vampire Weekend'
     },
     { src: '12-Wilco-EveryoneHides.mp3',
-      title: 'Wilco',
-      artist: 'Everyone Hides'
+      title: 'Everyone Hides',
+      artist: 'Wilco'
     },
     { src: '13-SharonVanEtten-Seventeen.mp3',
-      title: 'Sharon Van Etten',
-      artist: 'Seventeen'
+      title: 'Seventeen',
+      artist: 'Sharon Van Etten'
     },
     { src: '14-SleaterKinney-HurryOnHome.mp3',
-      title: 'Sleater-Kinney',
-      artist: 'Hurry On Home'
+      title: 'Hurry On Home',
+      artist: 'Sleater-Kinney'
     },
     { src: '15-PostMalone-Goodbyes.mp3',
-      title: 'Post Malone',
-      artist: 'Goodbyes (feat. Young Thug)'
+      title: 'Goodbyes (feat. Young Thug)',
+      artist: 'Post Malone'
     },
     { src: '16-Khalid-Talk.mp3',
-      title: 'Khalid (feat. Disclosure)',
-      artist: 'Talk'
+      title: 'Talk',
+      artist: 'Khalid (feat. Disclosure)'
     },
     { src: '17-JonasBrothers-OnlyHuman.mp3',
-      title: 'Jonas Brothers',
-      artist: 'Only Human'
+      title: 'Only Human',
+      artist: 'Jonas Brothers'
     },
     { src: '18-HarryStyles-WatermelonSugar.mp3',
-      title: 'Harry Styles',
-      artist: 'Watermelon Sugar'
+      title: 'Watermelon Sugar',
+      artist: 'Harry Styles'
     },
     { src: '19-RanveerSingh-ApnaTimeAayega.mp3',
-      title: 'Ranveer Singh, Dub Sharma',
-      artist: 'Apna Time Aayega'
+      title: 'Apna Time Aayega',
+      artist: 'Ranveer Singh, Dub Sharma'
     }
   ]
 };
