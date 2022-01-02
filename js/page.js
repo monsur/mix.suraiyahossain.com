c6283de509465d66be3d06e1862067a028b058a7
Monsur Hossain <monsur@gmail.com>
2022-01-02 17:19:45 -0600

    fix typo

--- js/page.js
+++ js/page.js
@@ -210,7 +210,7 @@ Page.prototype.updateTrack = function (track, nextTrack, action, isPlaying) {
       this.ui.setDownloadLink(track.getDownloadLink());
       this.ui.setSpotifyLink(track.getSpotifyLink());
       this.ui.setBackgroundColor(track.getBackgroundColor());
-      this.prevTrackYear == track.getYear();
+      this.prevTrackYear = track.getYear();
     }
     this.ui.setCurrentTrack(track);
     this.ui.setNextTrack(nextTrack);
