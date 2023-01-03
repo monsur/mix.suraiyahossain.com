4a7a82860a504e875895638dd641806504b2bce6
Monsur Hossain <monsur@gmail.com>
2023-01-02 23:31:03 -0600

    Using previous/next images.

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,6 +1,10 @@
 import { TrackData } from "./Types";
 
-function Player(props: { tracks: TrackData[]; currentTrackPos: number; setCurrentTrackPos: Function }) {
+function Player(props: {
+  tracks: TrackData[];
+  currentTrackPos: number;
+  setCurrentTrackPos: Function;
+}) {
   function handleNext() {
     let pos = props.currentTrackPos;
     if (pos < props.tracks.length - 1) {
@@ -17,7 +21,22 @@ function Player(props: { tracks: TrackData[]; currentTrackPos: number; setCurren
     props.setCurrentTrackPos(pos);
   }
 
-  return (<div><div onClick={handleNext}>NEXT</div> <div onClick={handlePrev}>PREV</div></div>);
+  return (
+    <div>
+      <img
+        alt="previous track"
+        title="previous track"
+        src="/images/prevtrack.png"
+        onClick={handlePrev}
+      />{" "}
+      <img
+        alt="next track"
+        title="next track"
+        src="/images/nexttrack.png"
+        onClick={handleNext}
+      />{" "}
+    </div>
+  );
 }
 
 export default Player;
