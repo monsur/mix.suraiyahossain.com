e440ef0046306d731256e5218425234be8ef1a26
Monsur Hossain <monsur@gmail.com>
2023-01-03 15:09:39 -0600

    simplifying key.

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -9,10 +9,11 @@ function Navigation(props: { minYear: number; maxYear: number }) {
 
   while (year >= props.minYear) {
     if (pos > 0) {
+      let spacer_key = year + "_spacer";
       if (pos % itemsPerLine === 0) {
-        items.push(<br key={`${year}_spacer`} />);
+        items.push(<br key={spacer_key} />);
       } else {
-        items.push(<span className="spacer" key={`${year}_spacer`}> | </span>);
+        items.push(<span className="spacer" key={spacer_key}> | </span>);
       }
     }
 
