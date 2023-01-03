5fff4c310fb4ceb2d3fc782482c84b99e54aa79d
Monsur Hossain <monsur@gmail.com>
2023-01-03 01:41:30 -0600

    calculating margin-right in code to prevent errors.

--- src/AlbumArtLarge.tsx
+++ src/AlbumArtLarge.tsx
@@ -7,13 +7,12 @@ function AlbumArtLarge(props: {
   width: number;
   baseUrl: string;
 }) {
-  // Keep this in sync with .back.margin-right in the css file.
-  // e.g. if margin-right is 20px, subtract 20/2 = 10px below.
-  let imgWidth = (Math.min(props.width, 900) - 10) / 2;
+  const backImgStyle = {marginRight: 20};
+  let imgWidth = (Math.min(props.width, 900) - backImgStyle.marginRight) / 2;
 
   return (
     <div className="AlbumArtLarge">
-      <img src={`${props.baseUrl}/${Globals.BACK_IMG}`} width={imgWidth} alt="album art back" className="back" />
+      <img src={`${props.baseUrl}/${Globals.BACK_IMG}`} width={imgWidth} alt="album art back" style={backImgStyle} />
       <img src={`${props.baseUrl}/${Globals.FRONT_IMG}`} width={imgWidth} alt="album art front" />
     </div>
   );
