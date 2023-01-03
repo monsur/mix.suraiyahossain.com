0a86004587dbb7e91dc7df9303a58bb0089ae0c9
Monsur Hossain <monsur@gmail.com>
2023-01-03 01:37:54 -0600

    Adding margin.

--- src/AlbumArtLarge.tsx
+++ src/AlbumArtLarge.tsx
@@ -7,11 +7,13 @@ function AlbumArtLarge(props: {
   width: number;
   baseUrl: string;
 }) {
-  let imgWidth = Math.min(props.width, 900) / 2;
+  // Keep this in sync with .back.margin-right.
+  // e.g. if margin-right is 20px, subtract 20/2 = 10px below.
+  let imgWidth = (Math.min(props.width, 900) - 10) / 2;
 
   return (
     <div className="AlbumArtLarge">
-      <img src={`${props.baseUrl}/${Globals.BACK_IMG}`} width={imgWidth} alt="album art back" />
+      <img src={`${props.baseUrl}/${Globals.BACK_IMG}`} width={imgWidth} alt="album art back" className="back" />
       <img src={`${props.baseUrl}/${Globals.FRONT_IMG}`} width={imgWidth} alt="album art front" />
     </div>
   );
