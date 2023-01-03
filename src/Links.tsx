31a55d7f2f2d88a0c0311ee83102dc087b53c634
Monsur Hossain <monsur@gmail.com>
2023-01-03 14:17:00 -0600

    Adding logger.

--- src/Links.tsx
+++ src/Links.tsx
@@ -3,16 +3,39 @@ import { YearData } from "./Types";
 import downloadIcon from "./assets/download.png";
 import spotifyIcon from "./assets/spotify.png";
 import "./Links.css";
+import Logger from "./Logger";
 
 function Links(props: { data: YearData }) {
-  let downloadLink = Globals.S3_PREFIX + props.data.year + "/" + props.data.title + ".zip";
+  let handleDownloadClick = () => {
+    Logger.log("links", "action", "download", props.data.year);
+    return true;
+  };
+
+  let handleSpotifyClick = () => {
+    Logger.log("links", "action", "spotify", props.data.year);
+    return true;
+  };
+
+  let downloadLink =
+    Globals.S3_PREFIX + props.data.year + "/" + props.data.title + ".zip";
+
   return (
     <div className="Links">
       <a href={downloadLink}>
-        <img src={downloadIcon} className="icon" alt="download"></img>
+        <img
+          src={downloadIcon}
+          className="icon"
+          alt="download"
+          onClick={handleDownloadClick}
+        ></img>
       </a>
       <a href={props.data.spotify}>
-        <img src={spotifyIcon} className="icon" alt="spotify"></img>
+        <img
+          src={spotifyIcon}
+          className="icon"
+          alt="spotify"
+          onClick={handleSpotifyClick}
+        ></img>
       </a>
     </div>
   );
---
+++ src/Logger.ts
@@ -0,0 +1,36 @@
+export default class Logger {
+  static logToServer = (category: string, action: string, label: string) => {
+    if (window["console"]) {
+      window["console"].log("event", action, {
+        event_category: category,
+        event_label: label,
+      });
+    }
+    if (window["gtag"]) {
+      window["gtag"]("event", action, {
+        event_category: category,
+        event_label: label,
+      });
+    }
+  };
+
+  static log = (
+    category: string,
+    action: string,
+    label?: string,
+    year?: number
+  ) => {
+    var logstr = "[" + category + ", " + action + "] ";
+    if (year) {
+      logstr += year + ", ";
+    }
+    if (label) {
+      logstr += label;
+    }
+    Logger.logToServer(category, action, logstr);
+  };
+
+  static error = (e: Error) => {
+    Logger.logToServer("error", "error", e.toString());
+  };
+}
