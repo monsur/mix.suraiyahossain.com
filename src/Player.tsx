acf63d824b26d8f15ecfa9db14a55361e6ef2c14
Monsur Hossain <monsur@gmail.com>
2023-01-02 23:25:33 -0600

    Adding simple prev/next buttons.

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,10 +1,23 @@
-function Player() {
+import { TrackData } from "./Types";
 
-  function handleClick() {
-  alert('boo');
+function Player(props: { tracks: TrackData[]; currentTrackPos: number; setCurrentTrackPos: Function }) {
+  function handleNext() {
+    let pos = props.currentTrackPos;
+    if (pos < props.tracks.length - 1) {
+      pos++;
+    }
+    props.setCurrentTrackPos(pos);
   }
 
-  return (<div onClick={handleClick}>This is a test.</div>)
+  function handlePrev() {
+    let pos = props.currentTrackPos;
+    if (pos > 0) {
+      pos--;
+    }
+    props.setCurrentTrackPos(pos);
+  }
+
+  return (<div><div onClick={handleNext}>NEXT</div> <div onClick={handlePrev}>PREV</div></div>);
 }
 
 export default Player;
