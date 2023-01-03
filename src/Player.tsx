e7e481a2ea80406b4b7378aacb2e72ef5886e886
Monsur Hossain <monsur@gmail.com>
2023-01-03 00:03:02 -0600

    Toggle play/pause.

--- src/Player.tsx
+++ src/Player.tsx
@@ -1,5 +1,5 @@
 import { TrackData } from "./Types";
-import { useState } from 'react';
+import { useState } from "react";
 
 function Player(props: {
   tracks: TrackData[];
@@ -25,6 +25,10 @@ function Player(props: {
     props.setCurrentTrackPos(pos);
   }
 
+  function handlePlayPause(val: boolean) {
+    setIsPlaying(val);
+  }
+
   return (
     <div>
       <audio id="audioplayer"></audio>
@@ -35,12 +39,23 @@ function Player(props: {
         src="/images/prevtrack.png"
         onClick={handlePrev}
       />
-      <img
-        className="playPause"
-        alt="play"
-        title="play"
-        src="/images/play.png"
-      />
+      {isPlaying ? (
+        <img
+          className="playPause"
+          alt="pause"
+          title="pause"
+          src="/images/pause.png"
+          onClick={() => handlePlayPause(false)}
+        />
+      ) : (
+        <img
+          className="playPause"
+          alt="play"
+          title="play"
+          src="/images/play.png"
+          onClick={() => handlePlayPause(true)}
+        />
+      )}
       <img
         className="nextTrack"
         alt="next track"
